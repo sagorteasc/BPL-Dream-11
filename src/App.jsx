1cba04dab8d3ace46bebeddb9bdde4cf66a86254
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2026-03-10 14:45:38 +0600

    add banner button functionality to increase navbar coins on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react'
 import './App.css'
 import Banner from './components/Banner/Banner'
 import Header from './components/Header/Header'
 
 function App() {
+  const [coins, setCoins] = useState(0);
+
+  const handleCoinIncrement = () => {
+    const newCoins = coins + 120000;
+    setCoins(newCoins);
+  }
+
   return (
     <>
       <div className='max-w-[1320px] mx-auto w-11/12 lg:w-full'>
-        <Header></Header>
-        <Banner></Banner>
+        <Header coins={coins}></Header>
+        <Banner handleCoinIncrement={handleCoinIncrement}></Banner>
       </div>
     </>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 import { TbCoinFilled } from "react-icons/tb";
-const Header = () => {
+const Header = ({ coins }) => {
     return (
         <nav className="pt-10 pb-6">
-            <div className="flex flex-col gap-3 justify-between items-center md:flex-row md:gap-0">
+            <div className="flex gap-3 justify-between items-center">
                 <img className="w-24 h-24" src="/src/assets/logo.png" alt="" />
-                <div className="flex items-center gap-3 flex-wrap md:gap-7 lg:gap-10">
-                    <ul className="text-[#13131370] flex items-center gap-3 md:gap-7 lg:gap-10">
+                <div className="flex items-center gap-2 md:gap-7 lg:gap-10">
+                    <ul className="text-[#13131370] hidden md:flex items-center gap-2 md:gap-7 lg:gap-10">
                         <li>Home</li>
                         <li>Fixture</li>
                         <li>Teams</li>
                         <li>Schedules</li>
                     </ul>
-                    <button className="flex items-center gap-1 font-semibold text-[#131313]"><span>0</span> Coins <TbCoinFilled /></button>
+                    <p className="flex items-center gap-1 font-semibold text-[#131313] border border-[#13131310] rounded-xl px-4 py-5 bg-slate-50"><span>{coins}</span> Coins <TbCoinFilled /></p>
                 </div>
             </div>
         </nav>
