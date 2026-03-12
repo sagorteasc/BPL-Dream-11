32d2321744d872e58405869be198d0aa6b983454
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2026-03-12 13:11:44 +0600

    fix footer alignment

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
 
                     <div className='text-white text-center mb-6 md:mb-0 md:text-start md:w-1/3 md:pl-24 lg:pl-40'>
                         <h3 className='font-semibold text-lg mb-1'>Quick Links</h3>
-                        <ul className='text-[#FFFFFF60] list-disc px-4 flex gap-6 md:block md:gap-0'>
+                        <ul className='text-[#FFFFFF60] flex justify-center gap-6 md:block md:list-disc md:list-inside'>
                             <li>Home</li>
                             <li>Services</li>
                             <li>About</li>
@@ -28,7 +28,7 @@ const Footer = () => {
                         <h3 className='font-semibold text-lg mb-1'>Subscribe</h3>
                         <p className='text-[#FFFFFF60] mb-3'>Subscribe to our newsletter for the latest updates.</p>
 
-                        <div className='px-4 flex items-center md:px-0'>
+                        <div className='px-4 flex w-full justify-center md:justify-normal md:px-0'>
                             <input
                                 className='text-[#13131340] text-sm py-4 pl-4 rounded-xl rounded-r-none'
                                 type="text"
@@ -42,7 +42,7 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className='border-t-gray-600'>
+                <div className='border-t border-gray-600'>
                     <p className='text-[#FFFFFF60] text-sm text-center py-5 md:text-base'>@2024 Your Company All Rights Reserved.</p>
                 </div>
             </div>
